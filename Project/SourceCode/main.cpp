31c214b0384edf29487a4762b4afe073618fed5c
Christoffer Modée <christoffer1996@gmail.com>
2017-09-09 00:29:35 +0200

    fixed gitignore and bad code

--- Project/SourceCode/main.cpp
+++ Project/SourceCode/main.cpp
@@ -7,7 +7,7 @@ int main()
 	NFileSettings::SetUpSettings();
 	CResources::InitializeInstance();
 
-	CProgram Program( sf::VideoMode( 1000, 800 ), "DVH", sf::Style::Titlebar + sf::Style::Close ); //Not sure if this is correct, but it works :P
+	CProgram Program( sf::VideoMode( 1000, 800 ), "DVH", sf::Style::Titlebar | sf::Style::Close ); //Not sure if this is correct, but it works :P
 	Program.Run();
 
 	return 0;
